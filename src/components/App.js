ee904ece0b7c7cbff436defad9e990e6f3af3bab
Jared Ivory <ivorj32@mail.sunysuffolk.edu>
2019-12-21 01:52:08 -0500

    Custome Router History, Refactoring, Fixed Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import history from '../history';
 
 import StreamCreate from './streams/StreamCreate';
-import StreamEdit from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
@@ -12,12 +13,16 @@ import Header from './Header';
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <Header></Header>
                 <div>
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route
+                        path="/streams/edit/:id"
+                        exact
+                        component={StreamEdit}
+                    />
                     <Route
                         path="/streams/delete"
                         exact
@@ -25,7 +30,7 @@ const App = () => {
                     />
                     <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 };
